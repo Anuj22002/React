448bd676229d78b6b1ee745b864028fc7fd874ee
Anuj <anujdharme15@gmail.com>
2022-08-30 18:33:14 +0530

    Updates

---
+++ precache-manifest.43ce93f56834587c0b99cf46e3e7fdc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "535bf88bdf97ccfdbc14",
+    "url": "/React/static/css/main.5b06ccc6.chunk.css"
+  },
+  {
+    "revision": "535bf88bdf97ccfdbc14",
+    "url": "/React/static/js/main.535bf88b.chunk.js"
+  },
+  {
+    "revision": "af08dc77e4e76da41e54",
+    "url": "/React/static/js/1.af08dc77.chunk.js"
+  },
+  {
+    "revision": "95a32ccb398f714557de",
+    "url": "/React/static/js/2.95a32ccb.chunk.js"
+  },
+  {
+    "revision": "5b02dd9b6e6f2a1d7795",
+    "url": "/React/static/js/runtime~main.5b02dd9b.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/React/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "86b0cf1fbfca164e7eb13d841c12f141",
+    "url": "/React/index.html"
+  }
+];
